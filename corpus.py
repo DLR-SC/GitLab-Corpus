30341bb39559ae3051e895c60ee76f53fb7ac3cf
Caricato, Emanuel <emanuel.caricato@dlr.de>
2021-04-10 11:35:41 +0200

    output corpus as json

--- corpus.py
+++ corpus.py
@@ -2,26 +2,47 @@ import json
 import click
 import gitlab
 
+corpus_dict = {"projects": [],
+               "groups": [],
+               "mergerequests": []
+               }
+
+
+def append_to_corpus(obj, category=""):
+    """This helper function appends a JSON object to the corpus."""
+    global corpus_dict
+    corpus_dict[category].append(obj)
+
 
 @click.command()
 @click.option('--config-path', default='./gitlab.cfg',
               help='Path to the config file')
 @click.option('--source',
               help='Name of the GitLab instance, you want to analyse, if not the default value of your configuration')
-@click.argument('out', type=click.File('w'), default='./out.txt')
-def cli(config_path, source, out):
+@click.option('--all-elements',
+              help='Get all elements available in the GitLab instance WARNING: This might take a long time and might '
+                   'cause problems for the server',
+              is_flag=True)
+@click.argument('out', type=click.File('w'), default='./out.json')
+def cli(config_path, source, all_elements, out):
     """This tool creates a corpus of all available software projects in a GitLab instance and writes its output to
-    the ``out`` file (default: ``./out.txt``)."""
+    the ``out`` file (default: ``./out.json``)."""
     gl = gitlab.Gitlab.from_config(source, config_path)
 
-    projects = gl.projects.list()
+    projects = gl.projects.list(all=all_elements)
     for project in projects:
-        click.echo(project, file=out)
+        append_to_corpus(project.attributes, category="projects")
 
-    groups = gl.groups.list()
+    groups = gl.groups.list(all=all_elements)
     for group in groups:
-        click.echo(group, file=out)
+        append_to_corpus(group.attributes, category="groups")
 
-    mergerequests = gl.mergerequests.list()
+    mergerequests = gl.mergerequests.list(all=all_elements)
     for mergerequest in mergerequests:
-        click.echo(mergerequest, file=out)
+        append_to_corpus(mergerequest.attributes, category="mergerequests")
+
+    json.dump(corpus_dict, out, indent=4)
+
+
+if __name__ == '__main__':
+    cli()
