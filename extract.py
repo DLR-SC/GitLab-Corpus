42827e2510ea48cdc5bf17bf57b6a9dd964b9ddd
Caricato, Emanuel <emanuel.caricato@smail.th-koeln.de>
2021-04-13 09:10:55 +0200

    refactor

--- extract.py
+++ extract.py
@@ -2,24 +2,23 @@
 # SPDX-License-Identifier: MIT
 
 def get_category(manager_string):
+    """This helper method extracts the category"""
     start_index = -1
     for i in range(0, 3):
         start_index = manager_string.find(".", start_index + 1)
     end_index = manager_string.find("Manager")
-    return manager_string[start_index+1:end_index] + "s"
+    return manager_string[start_index + 1:end_index] + "s"
 
 
 class Extractor:
     """This class represents the extraction module."""
 
     extracted_corpus = {"Projects": [],  # This object represents the output corpus and will exist only once
-                        "Groups": [],  # https://docs.python.org/3/tutorial/classes.html#class-and-instance-variables
-                        "MergeRequests": []
-                        }
+                        }                # https://docs.python.org/3/tutorial/classes.html#class-and-instance-variables
 
     def __init__(self, gitlab_manager):
         self.gl = gitlab_manager
-        self.managers = [self.gl.projects, self.gl.groups, self.gl.mergerequests]
+        self.managers = [self.gl.projects]
 
     def extract(self, all_elements):
         for manager in self.managers:
