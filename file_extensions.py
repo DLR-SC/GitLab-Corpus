d941b33ac4505f278322b41e8053beda0090b12d
Caricato, Emanuel <emanuel.caricato@smail.th-koeln.de>
2021-04-15 10:17:47 +0200

    add file extensions for source code files

---
+++ file_extensions.py
@@ -0,0 +1,19 @@
+import urllib.request
+import re
+
+urls = ["https://www.file-extensions.org/filetype/extension/name/source-code-and-script-files",
+        "https://www.file-extensions.org/filetype/extension/name/source-code-and-script-files/"
+        "sortBy/visits/order/desc/page/2",
+        "https://www.file-extensions.org/filetype/extension/name/source-code-and-script-files/"
+        "sortBy/visits/order/desc/page/3",
+        "https://www.file-extensions.org/filetype/extension/name/source-code-and-script-files/"
+        "sortBy/visits/order/desc/page/4"]
+
+for url in urls:
+    f = urllib.request.FancyURLopener({}).open(url)
+    content = f.read()
+
+    with open("extensions.txt", "a") as output:
+        content_str = str(content)
+        extensions = re.findall(r'"\w{5}3">([a-zA-Z0-9]*)<\/\w{6}>', content_str)
+        output.writelines(extension + ";" for extension in extensions)
