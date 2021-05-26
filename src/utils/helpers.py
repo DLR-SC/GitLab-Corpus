f16807f25d23d37ddd27ee123c46392f40a5454d
Caricato, Emanuel <emanuel.caricato@dlr.de>
2021-05-26 11:27:25 +0200

    add docstrings

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -2,14 +2,24 @@
 # SPDX-License-Identifier: MIT
 
 class Corpus:
+    """This helper class represents a corpus.
 
+    Methods:
+        __init__(self)
+    """
     def __init__(self):
+        """Corpus class constructor to initialize the object."""
         self.data = {"Projects": [],
                      }
 
 
 class Config:
+    """This helper class stores configuration options centralized, because they are used in different locations.
 
+    Methods:
+        __init__(self)
+    """
     def __init__(self):
+        """Config class constructor to initialize the object."""
         self.gl = None
         self.verbose = False
