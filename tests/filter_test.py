1520ea0317bf7bf456a88288d2b6325917e2a620
Caricato, Emanuel <emanuel.caricato@dlr.de>
2021-05-28 12:30:51 +0200

    add tests for all currently implemented methods

--- tests/filter_test.py
+++ tests/filter_test.py
@@ -69,7 +69,7 @@ language_test_corpus.data = {"Projects": [
     }
 ]}
 
-language_test_project = {
+test_project = {
     "id": 123,
     "languages": {
         "Python": 50.0,
@@ -145,7 +145,8 @@ def test_load_filters():
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
 
-    assert filter.filters == {"id": "==//30", "name": "example filter project", "issues_enabled": "==//true"}
+    assert filter.filters == {"atmost_languages": "", "id": "==//30", "name": "example filter project",
+                              "issues_enabled": "==//true"}
     assert filter.attributes == ["id", "name"]
     assert filter.atmost_languages == {"Python": "<=//100.0", "C": "<=//100.0"}
 
@@ -194,7 +195,7 @@ def test_check_languages_atleast_true():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("atleast_languages", language_test_project) is True
+    assert filter.check_languages("atleast_languages", test_project) is True
 
 
 def test_check_languages_atleast_false():
@@ -211,7 +212,7 @@ def test_check_languages_atleast_false():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("atleast_languages", language_test_project) is False
+    assert filter.check_languages("atleast_languages", test_project) is False
 
 
 def test_check_languages_atmost_true():
@@ -229,7 +230,7 @@ def test_check_languages_atmost_true():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("atmost_languages", language_test_project) is True
+    assert filter.check_languages("atmost_languages", test_project) is True
 
 
 def test_check_languages_atmost_false():
@@ -245,7 +246,7 @@ def test_check_languages_atmost_false():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("atmost_languages", language_test_project) is False
+    assert filter.check_languages("atmost_languages", test_project) is False
 
 
 def test_check_languages_any_true():
@@ -261,7 +262,7 @@ def test_check_languages_any_true():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("any_languages", language_test_project) is True
+    assert filter.check_languages("any_languages", test_project) is True
 
 
 def test_check_languages_any_false():
@@ -276,7 +277,7 @@ def test_check_languages_any_false():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("any_languages", language_test_project) is False
+    assert filter.check_languages("any_languages", test_project) is False
 
 
 def test_check_languages_explicit_true():
@@ -294,7 +295,7 @@ def test_check_languages_explicit_true():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("explicit_languages", language_test_project) is True
+    assert filter.check_languages("explicit_languages", test_project) is True
 
 
 def test_check_languages_explicit_false():
@@ -310,4 +311,119 @@ def test_check_languages_explicit_false():
     filter = Filter(False, language_test_corpus, False, "")
     with mock.patch("builtins.open", mocked_filter_file, create=True):
         filter.load_filters(filter_file="mocked_filters.yaml")
-    assert filter.check_languages("explicit_languages", language_test_project) is False
+    assert filter.check_languages("explicit_languages", test_project) is False
+
+
+def test_filter_project_true_1():
+    mocked_filters = """
+            filters:
+                - any_languages:
+                    - C: "<=//100.0"
+                    - Python: "<=//100.0"
+
+            attributes:
+            """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, Corpus(), False, "")
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    assert filter.filter_project(test_project) is True
+
+
+def test_filter_project_true_2():
+    mocked_filters = """
+            filters:
+                - id: "==//123"
+            attributes:
+            """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, Corpus(), False, "")
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    assert filter.filter_project(test_project) is True
+
+
+def test_filter_project_false():
+    mocked_filters = """
+            filters:
+                - any_languages:
+                    - Ada: "<=//100.0"
+            attributes:
+            """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, Corpus(), False, "")
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    assert filter.filter_project(test_project) is False
+
+
+def test_filter_with_filters():
+    mocked_filters = """
+            filters:
+                - id: "<//100"
+            attributes:
+    """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, corpus, False, "")
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    filter.filter()
+    assert filter.filtered_corpus.data == corpus.data
+
+
+def test_filter_with_attributes():
+    mocked_filters = """
+        filters:
+        attributes:
+            - id
+    """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, corpus, False, "")
+    filter.filtered_corpus = Corpus()
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    filter.filter()
+    assert filter.filtered_corpus.data == {"Projects": [
+        {
+            'id': 1
+        }
+    ]}
+
+
+def test_filter_with_both():
+    mocked_filters = """
+            filters:
+                - id: "<//100"
+            attributes:
+                - id
+        """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, corpus, False, "")
+    filter.filtered_corpus = Corpus()
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    filter.filter()
+    assert filter.filtered_corpus.data == {"Projects": [
+        {
+            'id': 1
+        }
+    ]}
+
+
+def test_filter_no_filters():
+    mocked_filters = """
+            filters:
+            attributes:
+    """
+    mocked_filter_file = mock.mock_open(read_data=mocked_filters)
+    filter = Filter(False, corpus, False, "")
+    filter.filtered_corpus = Corpus()
+    with mock.patch("builtins.open", mocked_filter_file, create=True):
+        filter.load_filters(filter_file="mocked_filters.yaml")
+    filter.filter()
+    assert filter.filtered_corpus.data == corpus.data
+
+
+if __name__ == '__main__':
+    test_filter_with_filters()
+    test_filter_with_attributes()
