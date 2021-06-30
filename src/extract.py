4fc39803ee213185f856930ed9e898bc4d337455
Caricato, Emanuel <emanuel.caricato@smail.th-koeln.de>
2021-06-30 11:04:14 +0200

    add tests and implementation for contributors
    closes #21

--- src/extract.py
+++ src/extract.py
@@ -38,6 +38,9 @@ class Extractor:
                         click.echo("{} projects found.".format(bar.length))
                     for project in bar:
                         project_dict = project.attributes
+                        contributors = set()
+                        external_contributors = set()
+                        contributor_names = set()
 
                         if project_dict['visibility'] != "private":
 
@@ -46,28 +49,54 @@ class Extractor:
 
                             project_dict['languages'] = project.languages()
 
-                            members = project.members.list(all=True)
-                            if len(members) > 0:
-                                project_dict['members'] = members[0].attributes
+                            members = project.members_all.list(all=True)
 
                             try:
                                 commits = project.commits.list(all=True)
                                 commit_list = []
                                 for commit in commits:
-                                    commit_list.append(commit.attributes)
+                                    commit_dict = commit.attributes
+                                    commit_list.append(commit_dict)
+                                    contributor_names.add(commit_dict['author_name'])
+
                                 if len(commit_list) > 0:
                                     project_dict['commits'] = commit_list
                                     project_dict['first_commit'] = commit_list.__getitem__(len(commit_list) - 1)
                                     project_dict['last_commit'] = commit_list.__getitem__(0)
                             except gitlab.exceptions.GitlabListError:
-                                pass    # some projects do not have any commits, which leads to this error
+                                pass  # some projects do not have any commits, which leads to this error
+
+                            for member in members:
+                                try:
+                                    surname, forename = member.attributes['name'].split(', ')
+                                    member_name = forename + ' ' + surname
+                                except ValueError:
+                                    member_name = member.attributes['name']
+                                to_be_removed = set()
+                                for name in contributor_names:
+                                    if member_name == name:
+                                        contributors.add(member.attributes['username'])
+                                        to_be_removed.add(name)
+                                for name in to_be_removed:
+                                    contributor_names.remove(name)
+                            external_contributors.update(contributor_names)
 
                             issues = project.issues.list(all=True)
                             issue_list = []
                             for issue in issues:
-                                issue_list.append(issue.attributes)
+                                issue_dict = issue.attributes
+                                issue_list.append(issue_dict)
+                                contributors.add(issue_dict['author']['username'])
+                                for assignee in issue_dict['assignees']:
+                                    contributors.add(assignee['username'])
                             if len(issue_list) > 0:
                                 project_dict['issues'] = issue_list
+
+                            if len(contributors) > 0:
+                                project_dict['contributors'] = list(contributors)
+
+                            if len(external_contributors) > 0:
+                                project_dict['external_contributors'] = list(external_contributors)
 
                             try:
                                 project_dict['files'] = project.repository_tree(ref=project_dict['default_branch'])
