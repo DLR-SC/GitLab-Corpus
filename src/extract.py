b256b2a102c76c608cf97ab7ab5ff4dd665c4cdf
Caricato, Emanuel <emanuel.caricato@dlr.de>
2021-08-19 08:31:59 +0200

    refactor extraction
    closes #22

--- src/extract.py
+++ src/extract.py
@@ -5,6 +5,117 @@ import gitlab
 from gitlab.v4.objects import ProjectManager
 
 
+def get_users(project):
+    try:
+        users = project.users.list()
+        user_list = []
+        for user in users:
+            user_dict = user.attributes
+            user_list.append(user_dict)
+
+        if len(user_list) > 0:
+            return user_list
+    except gitlab.exceptions.GitlabListError:
+        pass
+
+
+def get_commits(project):
+    try:
+        commits = project.commits.list(all=True)
+        commit_list = []
+        for commit in commits:
+            commit_dict = commit.attributes
+            commit_list.append(commit_dict)
+
+        if len(commit_list) > 0:
+            return commit_list, commit_list.__getitem__(len(commit_list) - 1), commit_list.__getitem__(0)
+    except gitlab.exceptions.GitlabListError:
+        pass  # some projects do not have any commits, which leads to this error
+
+
+def get_contributors(project):
+    try:
+        contributors = project.repository_contributors()
+        return contributors
+    except gitlab.exceptions.GitlabGetError:
+        pass  # when no contributors exist
+
+
+def get_issues(project):
+    issues = project.issues.list(all=True)
+    issue_list = []
+    for issue in issues:
+        issue_dict = issue.attributes
+        issue_list.append(issue_dict)
+    if len(issue_list) > 0:
+        return issue_list
+
+
+def get_mergerequests(project):
+    try:
+        mergerequests = project.mergerequests.list(state='all')
+        mr_list = []
+        for mr in mergerequests:
+            mr_dict = mr.attributes
+            mr_list.append(mr_dict)
+        if len(mr_list) > 0:
+            return mr_list
+    except gitlab.exceptions.GitlabListError:
+        pass  # occurs when no mergerequests exist
+
+
+def get_pipelinestatistics(project):
+    try:
+        pipelines = project.pipelines.list()
+        pipelines_dict = {"total": 0, "successful": 0, "failed": 0, "canceled": 0, "pending": 0}
+        for pipeline in pipelines:
+            status = pipeline.attributes['status']
+            if status == "success":
+                pipelines_dict['successful'] += 1
+            elif status == 'failed':
+                pipelines_dict['failed'] += 1
+            elif status == 'canceled':
+                pipelines_dict['canceled'] += 1
+            elif status == 'pending':
+                pipelines_dict['pending'] += 1
+        pipelines_dict['total'] = pipelines_dict['successful'] + pipelines_dict['failed'] \
+                                  + pipelines_dict['canceled'] + pipelines_dict['pending']
+        return pipelines_dict
+    except gitlab.exceptions.GitlabListError:
+        pass
+
+
+def get_milestones(project):
+    try:
+        milestones = project.milestones.list()
+        ms_list = []
+        for ms in milestones:
+            ms_dict = ms.attributes
+            ms_list.append(ms_dict)
+        if len(ms_list) > 0:
+            return ms_list
+    except gitlab.exceptions.GitlabListError:
+        pass
+
+
+def get_rootdir(project, default_branch):
+    try:
+        return project.repository_tree(ref=default_branch)
+    except (gitlab.exceptions.GitlabGetError, gitlab.exceptions.GitlabHttpError, KeyError):
+        pass
+
+
+def get_projectstatistics(project, verbose, name):
+    try:
+        return project.additionalstatistics.get().attributes
+    except gitlab.exceptions.GitlabGetError:
+        if verbose:
+            click.echo("\n Project statistics for project {} could not be fetched. You might "
+                       "need write access to fix this.".format(name))
+        else:
+            pass
+
+
 class Extractor:
     """This class provides a method to extract projects of GitLab instance.
 
@@ -50,108 +161,34 @@ class Extractor:
                             project_dict['languages'] = project.languages()
 
                             # extract members of the project
-                            try:
-                                users = project.users.list()
-                                user_list = []
-                                for user in users:
-                                    user_dict = user.attributes
-                                    user_list.append(user_dict)
-
-                                if len(user_list) > 0:
-                                    project_dict['users'] = user_list
-                            except gitlab.exceptions.GitlabListError:
-                                pass
+                            project_dict['users'] = get_users(project)
 
                             # extract commits
-                            try:
-                                commits = project.commits.list(all=True)
-                                commit_list = []
-                                for commit in commits:
-                                    commit_dict = commit.attributes
-                                    commit_list.append(commit_dict)
+                            project_dict['commits'], project_dict['first_commit'], project_dict['last_commit'] = \
+                                get_commits(project)
 
-                                if len(commit_list) > 0:
-                                    project_dict['commits'] = commit_list
-                                    project_dict['first_commit'] = commit_list.__getitem__(len(commit_list) - 1)
-                                    project_dict['last_commit'] = commit_list.__getitem__(0)
-                            except gitlab.exceptions.GitlabListError:
-                                pass  # some projects do not have any commits, which leads to this error
-
-                            try:
-                                contributors = project.repository_contributors()
-                                project_dict['contributors'] = contributors
-                            except gitlab.exceptions.GitlabGetError:
-                                pass  # when no contributors exist
+                            # extract contributors
+                            project_dict['contributors'] = get_contributors(project)
 
                             # extract all issues of a project and add further contributors to list
-                            issues = project.issues.list(all=True)
-                            issue_list = []
-                            for issue in issues:
-                                issue_dict = issue.attributes
-                                issue_list.append(issue_dict)
-                            if len(issue_list) > 0:
-                                project_dict['issues'] = issue_list
+                            project_dict['issues'] = get_issues(project)
 
                             # extract all merge requests
-                            try:
-                                mergerequests = project.mergerequests.list(state='all')
-                                mr_list = []
-                                for mr in mergerequests:
-                                    mr_dict = mr.attributes
-                                    mr_list.append(mr_dict)
-                                if len(mr_list) > 0:
-                                    project_dict['mergerequests'] = mr_list
-                            except gitlab.exceptions.GitlabListError:
-                                pass  # occurs when no mergerequests exist
+                            project_dict['mergerequests'] = get_mergerequests(project)
 
                             # extract pipeline statistics
-                            try:
-                                pipelines = project.pipelines.list()
-                                pipelines_dict = {"total": 0, "successful": 0, "failed": 0, "canceled": 0, "pending": 0}
-                                for pipeline in pipelines:
-                                    status = pipeline.attributes['status']
-                                    if status == "success":
-                                        pipelines_dict['successful'] += 1
-                                    elif status == 'failed':
-                                        pipelines_dict['failed'] += 1
-                                    elif status == 'canceled':
-                                        pipelines_dict['canceled'] += 1
-                                    elif status == 'pending':
-                                        pipelines_dict['pending'] += 1
-                                pipelines_dict['total'] = pipelines_dict['successful'] + pipelines_dict['failed'] \
-                                                          + pipelines_dict['canceled'] + pipelines_dict['pending']
-                                project_dict['pipelines'] = pipelines_dict
-                            except gitlab.exceptions.GitlabListError:
-                                pass
+                            project_dict['pipelines'] = get_pipelinestatistics(project)
 
                             # extract milestones
-                            try:
-                                milestones = project.milestones.list()
-                                ms_list = []
-                                for ms in milestones:
-                                    ms_dict = ms.attributes
-                                    ms_list.append(ms_dict)
-                                if len(ms_list) > 0:
-                                    project_dict['milestones'] = ms_list
-                            except gitlab.exceptions.GitlabListError:
-                                pass
+                            project_dict['milestones'] = get_milestones(project)
 
                             # extract the main directory of the project
-                            try:
-                                project_dict['files'] = project.repository_tree(ref=project_dict['default_branch'])
-                            except (gitlab.exceptions.GitlabGetError, gitlab.exceptions.GitlabHttpError, KeyError):
-                                project_dict['files'] = "None"
+                            project_dict['files'] = get_rootdir(project, project_dict['default_branch'])
 
                             # try to extract project statistics (only works for projects where the user has write
                             # access)
-                            try:
-                                project_dict['project_statistics'] = project.additionalstatistics.get().attributes
-                            except gitlab.exceptions.GitlabGetError:
-                                if self.verbose:
-                                    click.echo("\n Project statistics for project {} could not be fetched. You might "
-                                               "need write access to fix this.".format(project_dict["name"]))
-                                else:
-                                    pass
+                            project_dict['project_statistics'] = get_projectstatistics(project, self.verbose,
+                                                                                       project_dict['name'])
 
                             # add all extracted data to the corpus
                             self.corpus.data["Projects"].append(project_dict)
